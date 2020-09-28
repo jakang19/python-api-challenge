39b6323e7120934aeb9682c63f5e2a03944a9811
jakang <jakang@wustl.edu>
2020-09-28 14:51:29 -0700

    generate plots

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ec1eb98758e3a13eac17a92fe795ff36"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCyeCemNi1M37gknjnJodbczbsmVWIkukQ"
